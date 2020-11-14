d5c3e1ffde6a981b5ac9cc8b2008be85c715953d
OhSeungYoung <osy0056@khu.ac.kr>
2020-11-14 18:42:17 +0900

    feat: Modify node api path

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,37 @@
-const http = require('http');
-const url = require('url');
-const objJson = require('./test.json');
+const mysql = require("mysql");
+const http = require("http");
+const url = require("url");
 
-http.createServer((req, res) => {
-  const path = url.parse(req.url, true).pathname;
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "me",
+  password: "secret",
+  database: "my_db",
+});
 
-  if (req.method === 'GET') {
+const parseJson = (res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  const objJson = require("./test.json");
+  const testJson = JSON.stringify(objJson);
+  res.end(testJson);
+};
 
-    if (path === '/node') {
-      res.writeHead(200,{'Content-Type':'application/json'});
-      const testJson = JSON.stringify(objJson)
-      res.end(testJson);
-    } else {
-      res.statusCode = 404;
-      res.end('no adresss');
+http
+  .createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname;
+
+    if (req.method === "GET") {
+      if (path === "/json") {
+        parseJson(res);
+      } else if (path === "/db") {
+      } else {
+        res.statusCode = 404;
+        res.end("no adresss");
+      }
+
+      // if (path === "/db") {
+      //   res.writeHead(200, { "Content-Type": "application/json" });
+      // }
     }
-  }
-}).listen(8081);
+  })
+  .listen(8081);
