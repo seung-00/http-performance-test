0053de0374c677d3a1a42045b366d1c61cd26876
OhSeungYoung <osy0056@khu.ac.kr>
2020-12-29 02:28:45 +0900

    feat: Add graphs

--- src/client/tps_hist.py
+++ src/client/tps_hist.py
@@ -8,29 +8,28 @@ def draw_output(plot):
                  '%d' % int(height), ha='center', va='bottom', size='12')
 
 
-results = {'native': [[74.82, 53.21], [69.62, 55.15], [74.95, 57.98]], 'vm': [[33.83, 24.54], [29.04, 21.17], [34.14, 24.54]],
-           'container': [[55.98, 45.99], [50.87, 42.83], [55.20, 46.90]]}
+results = {'native': [[6.06, 12.65], [7.10, 12.15], [5.33, 10.52]], 'vm': [[25.08, 36.13], [29.37, 38.58], [23.89, 30.66]],
+'container': [[7.69, 13.49], [10.84, 14.76], [5.47, 12.81]]}
 
-x = [['js_json', 'js_query'], ['python_json',
-                               'python_query'], ['go_json', 'go_query']]
-for env in ['native', 'vm', 'container']:
-    y = results[env]
+x = [['native'], ['container'], ['vm']]
+for env in ['memory usage']:
+    y = [17, 19, 28]
     plot1 = plt.bar(x[0], height=y[0],  width=0.6,
-                    color="red", label='javascript')
+                    color="red", label='native')
     draw_output(plot1)
 
     plot2 = plt.bar(x[1], height=y[1],
-                    width=0.6, color="blue", label='python')
+                    width=0.6, color="blue", label='container')
     draw_output(plot2)
 
     plot3 = plt.bar(x[2], height=y[2],
-                    width=0.6, color="green", label='go')
+                    width=0.6, color="green", label='vm')
     draw_output(plot3)
 
     plt.title(f'{env}', fontdict={
         'family': 'sans-serif', 'weight': 'bold', 'size': 15})
 
-    plt.ylabel("TPS", size=15)
+    plt.ylabel("memory usage (%)", size=15)
     plt.legend()
     plt.savefig(f'{env}.png', dpi=200)
     plt.close()
